87c5997daa60775984a9e458a8195bb12f7e73de
FrancisLoayza <faloayza@espol.edu.ec>
2024-06-23 00:53:31 -0500

    modificacion

--- src/main.java
+++ src/main.java
@@ -1,9 +1,9 @@
+package src;
 public class main {
     public static void main(String[] args) {
         System.out.println("Hola");
         Interfaz in1 = new Interfaz();
-        in1.inicio();        
-        System.out.println("Hola como estas");
+        in1.inicio();
         
     }
 }
