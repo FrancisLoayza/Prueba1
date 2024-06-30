e785978c0ca1299734f52f7f66499fd11fec67ee
JoeyBustamante <149977232+JoeyBustamante@users.noreply.github.com>
2024-06-29 21:24:12 -0500

    Update main.java

--- src/main.java
+++ src/main.java
@@ -11,15 +11,7 @@ public class main {
     Interfaz interfaz= new Interfaz();
 
     public static void main(String[] args) {
-        /*Usuario autor=new Autor("Francis", "Loayza", "loayzafrancis@gmail.com", Rol.A, null, 0, null, null, null);
-        Sistema.AgergarListaUsuario(autor);
-        System.out.println(Sistema.listaUsuario.toString());
-        Sistema.registrarDatoUsuario();
-        System.out.println("Ingrese su Usuario:");
-        Scanner sc=new Scanner(System.in);
-        String usuario=sc.nextLine();
-        System.out.println("Ingrese su Contrasena:");
-        String contra=sc.nextLine(); */
+        
         Interfaz.inicio();
     }
 }
